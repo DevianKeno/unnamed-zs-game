d6ae5646ec28e3bf27a28ac00c81734fe398f8ff
deviank <tensaidevi@gmail.com>
2024-07-23 07:03:23 +0800

    Added gun smoke

--- Assets/Scripts/Items/Weapons/GunMuzzleController.cs
+++ Assets/Scripts/Items/Weapons/GunMuzzleController.cs
@@ -6,28 +6,28 @@ namespace UZSG.Systems
     public class GunMuzzleController : MonoBehaviour
     {
         public float FlashIntensity = 1f;
+        public Material FlashMaterial;
+        public Material SmokeMaterial;
+
         [SerializeField] ParticleSystem muzzleFlashParticle;
         [SerializeField] ParticleSystem muzzleSmokeParticle;
         [SerializeField] Light muzzleFlashLight;
 
+        void Start()
+        {
+            if (FlashMaterial != null) muzzleFlashParticle.GetComponent<ParticleSystemRenderer>().material = FlashMaterial;
+            if (SmokeMaterial != null) muzzleSmokeParticle.GetComponent<ParticleSystemRenderer>().material = SmokeMaterial;
+        }
+
         public void OnFire()
         {
             muzzleFlashParticle?.Play();
-            // muzzleSmokeParticle?.Play();
-
-            StartCoroutine(StopMuzzleFlashParticles());
-
+            muzzleSmokeParticle?.Play();
             LeanTween.value(gameObject, FlashIntensity, 0, 0.1f)
             .setOnUpdate((float i) => 
             {
                 muzzleFlashLight.intensity = i;
             });
-        }
-
-        IEnumerator StopMuzzleFlashParticles()
-        {
-            yield return new WaitForSeconds(0.1f);
-            muzzleFlashParticle?.Stop();
         }
     }
 }

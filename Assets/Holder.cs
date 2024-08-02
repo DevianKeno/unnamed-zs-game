d08bd23922cf64254225be3fc95113e0acb3b246
Annalurhge <annalurhge@gmail.com>
2024-08-02 10:00:44 +0800

    world event shit

---
+++ Assets/Holder.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UZSG.World.Weather
+{
+    public class RainDataHolder : MonoBehaviour
+    {
+        public WeatherData WeatherData;
+    }
+}
+

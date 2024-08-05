f5dd6a0f5d4090c609a89982b7b3d01634cb35e6
recoder99 <30973326+recoder99@users.noreply.github.com>
2024-08-05 09:27:45 +0800

    overload function for crafting added:
    -CraftItems() can now accept RecipeData as an input

--- Assets/Scripts/Crafting/Crafter.cs
+++ Assets/Scripts/Crafting/Crafter.cs
@@ -78,6 +78,41 @@ namespace UZSG.Crafting
         }
 
         /// <summary>
+        /// Consumes items in the container and returns an item whenever the required resource is available. 
+        /// Returns a dictionary of slots pertaining to the recipe. 
+        /// </summary>
+        protected bool CheckMaterialAvailability(Item item, RecipeData recipe, Dictionary<Item, List<ItemSlot>> dictSlots){
+
+            // if (recipeIndex > item.Data.Recipes.Count){
+            //     print("Recipe index out of bound");
+            //     return false;
+            // }
+
+            foreach (Item material in recipe.Materials)
+            {
+                var materialSlots = new List<ItemSlot>();
+                
+                int _totalItemCount = 0;
+
+                foreach (Container container in containers)
+                {
+                    var tempSlots = new List<ItemSlot>();
+                    _totalItemCount += container.ItemCount(material, out tempSlots);
+                    materialSlots.AddRange(tempSlots);
+                }
+
+                if (_totalItemCount < material.Count){
+                    print("Materials required does not match the current container");
+                    return false;
+                } 
+
+                dictSlots.Add(material, materialSlots);
+            }
+
+            return true;
+        }
+
+        /// <summary>
         /// Consumes items from the container
         /// </summary>
         protected virtual void TakeItems(Item item, int recipeIndex, Dictionary<Item, List<ItemSlot>> dictSlots)
@@ -114,6 +149,43 @@ namespace UZSG.Crafting
         }
 
         /// <summary>
+        /// Consumes items from the container
+        /// </summary>
+        protected virtual void TakeItems(Item item, RecipeData recipe, Dictionary<Item, List<ItemSlot>> dictSlots){
+
+            foreach (Item material in recipe.Materials)
+            {
+                int remainingCount = material.Count;
+
+                foreach (ItemSlot slot in dictSlots[material])
+                {
+                    /*
+                        comparator checks the difference of the remaining count and the
+                        required count of the material.
+                    */                    
+                    if (remainingCount <= 0)
+                    {
+                        break;
+                    }
+
+                    int comparator = slot.Item.Count - remainingCount;
+
+                    if (comparator > 0)
+                    {
+                        remainingCount -= material.Count;
+                        slot.TakeItems(material.Count);
+                    }
+                    else 
+                    {
+                        remainingCount -= slot.Item.Count;
+                        slot.TakeAll();
+                    }
+
+                }
+            }
+        }
+
+        /// <summary>
         /// Consumes items in the container and returns an item whenever the required resource is available
         /// </summary>
         public virtual void CraftItem(Item item, int recipeIndex)
@@ -136,6 +208,20 @@ namespace UZSG.Crafting
         public bool TryCraftRecipe(RecipeData recipe)
         {
             return false;
+        }
+
+        public virtual void CraftItem(Item item, RecipeData recipe)
+        {
+            var dictSlots = new Dictionary<Item, List<ItemSlot>>();
+            if ( !CheckMaterialAvailability(item, recipe, dictSlots)) {
+                return;
+            }
+
+            TakeItems(item, recipe, dictSlots);
+
+            foreach (Container container in containers){
+                if (container.TryPutNearest(new Item(item))) break;
+            }
         }
     }
 }

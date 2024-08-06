3a49cc0de6023659033bd2482c533198fa448f1a
jp-jp-123 <jpbeltran098@gmail.com>
2024-08-06 16:55:12 +0800

    Ground Material Detection
    added raycast object to player entity ground checker. Added material detection script

--- Assets/Scripts/PlayerCore/GroundChecker.cs
+++ Assets/Scripts/PlayerCore/GroundChecker.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using UZSG.Entities;
+using UZSG.Interactions;
 
 namespace UZSG.Players
 {
     public class GroundChecker : MonoBehaviour
     {
         [SerializeField] bool _isGrounded;
+        public LayerMask mask;
+        public Transform rayStart;
+        public float rayCastRange;
+
+        RaycastHit hit;
+        Material material;
+        int _limit;
+        int _submesh;
+        int _numIndices;
+
         public bool IsGrounded => _isGrounded;
 
         [SerializeField] BoxCollider boxCollider;
@@ -17,11 +30,44 @@ namespace UZSG.Players
         public void OnTriggerStay(Collider other)
         {
             _isGrounded = true;
+            GroundMaterialDetection();
         }
 
         public void OnTriggerExit(Collider other)
         {
             _isGrounded = false;
+        }
+
+        public void GroundMaterialDetection()
+        {
+            if (Physics.Raycast(rayStart.position, rayStart.transform.up * -1 , out hit, rayCastRange, mask))
+            {
+                MeshCollider collider = hit.collider as MeshCollider;
+                // Remember to handle case where collider is null because you hit a non-mesh primitive...
+
+                if(collider == null)
+                {
+                    material = null;
+                }
+                else
+                {
+                    Mesh mesh = collider.sharedMesh;
+
+                    // There are 3 indices stored per triangle
+                    _limit = hit.triangleIndex * 3;
+                    
+                    for (_submesh = 0; _submesh < mesh.subMeshCount; _submesh++)
+                    {
+                        _numIndices = mesh.GetTriangles(_submesh).Length;
+                        if (_numIndices > _limit)
+                            break;
+
+                        _limit -= _numIndices;
+                    }
+
+                    material = collider.GetComponent<MeshRenderer>().sharedMaterials[_submesh];
+                }
+            }
         }
     }
 }

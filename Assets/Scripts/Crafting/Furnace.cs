a6e32a88db9793ab88b8ff781d015dc083016f69
recoder99 <30973326+recoder99@users.noreply.github.com>
2024-08-08 16:23:05 +0800

    added logic to prepare cooking
    fuck

--- Assets/Scripts/Crafting/Furnace.cs
+++ Assets/Scripts/Crafting/Furnace.cs
@@ -1,19 +1,18 @@
 using System;
+using System.Collections.Generic;
 using UnityEditor.Build;
 using UnityEngine;
 using UZSG.Data;
+using UZSG.Inventory;
 using UZSG.Items;
 
 namespace UZSG.Crafting
 {
     public class Furnace : FuelBasedCrafting
     {
-        public Container ConsumableContainer = new(3);
+        public Container Dishes = new(5);
+        public Container InputContainer;
         public int FurnaceCapacity = 3;
-
-        public void Init(){
-            // InputContainers.Add(new Container(5));
-        }
 
         protected bool isFurnaceFull(){
             if(craftingRoutineList.Count >= FurnaceCapacity){
@@ -22,10 +21,18 @@ namespace UZSG.Crafting
             return false;
         }
 
-        public void StartCookingConsumable(Item item){
+        public void StartCooking(RecipeData recipe){
             if (!isFurnaceFull()){
                 print("Furnace is still busy");
+                return;
             }
+
+            if(!CheckMaterialAvailability(recipe, InputContainer)){
+                print("Not enough materials");
+                return;
+            };
+            var _ingredients = TakeItems(recipe, InputContainer);
+            var _beingCooked = new CraftingRoutine(recipe, _ingredients, Dishes);
         }
     }
 }

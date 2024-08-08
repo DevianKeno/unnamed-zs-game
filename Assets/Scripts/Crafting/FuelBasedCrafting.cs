19b06c94c0ed6589b7ab75fa5487f211abfd5ee9
recoder99 <30973326+recoder99@users.noreply.github.com>
2024-08-08 11:49:41 +0800

    Fuel based

--- Assets/Scripts/Crafting/FuelBasedCrafting.cs
+++ Assets/Scripts/Crafting/FuelBasedCrafting.cs
@@ -1,19 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 using UZSG.Items;
 
 namespace UZSG.Crafting
 {
     public class FuelBasedCrafting : Crafter
     {
-        Item Fuel;
-        
-        public void setFuel(Item item)
-        {
-            
+        public float FuelRemaining = 0;
+        Container FuelContainer;
+
+        protected bool isFuelRemainingAvailable() {
+            if (FuelRemaining > 0) {
+                return true;
+            }
+            return false;
         }
 
-        public void consumeFuel()
+        protected bool isFuelAvailable() {
+            if (FuelContainer.Slots[0].IsEmpty){
+                return false;
+            }
+            return true;
+        }
+
+        public void ConsumeFuel()
         {
-            
+            if (isFuelAvailable() || isFuelRemainingAvailable()) return;
+            FuelContainer.TakeItems(0, 1);
+            StartCoroutine(BurnFuel());
+        }
+
+        IEnumerator BurnFuel(){
+            while (isFuelRemainingAvailable())
+            {
+                yield return new WaitForSeconds(1);
+                FuelRemaining -= 1;
+                if(!isFuelRemainingAvailable())
+                {
+                    ConsumeFuel();
+                }
+            }
         }
     }
 }
---
+++ Assets/Scripts/Crafting/Furnace.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEditor.Build;
+using UnityEngine;
+using UZSG.Data;
+using UZSG.Items;
+
+namespace UZSG.Crafting
+{
+    public class Furnace : FuelBasedCrafting
+    {
+        public Container ConsumableContainer = new(3);
+        public int FurnaceCapacity = 3;
+
+        public void Init(){
+            InputContainers.Add(new Container(5));
+        }
+
+        protected bool isFurnaceFull(){
+            if(craftingRoutineList.Count >= FurnaceCapacity){
+                return true;
+            }
+            return false;
+        }
+
+        public void StartCookingConsumable(Item item){
+            if (!isFurnaceFull()){
+                print("Furnace is still busy");
+            }
+        }
+    }
+}
+
+    

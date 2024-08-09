78172ba0bba1b8904b6496d925d42978b3ad4920
jp-jp-123 <jpbeltran098@gmail.com>
2024-08-09 12:02:54 +0800

    Compass Edge Fade out
    added 2D Rect Mask in each compass elements for fade out

---
+++ Assets/Scripts/UI/Player HUD/ThreeWayGradient.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThreeWayGradient : MonoBehaviour
+{
+    public Color firstColor;
+    public Color secondColor;
+    public Color thirdColor;
+
+    public RawImage image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

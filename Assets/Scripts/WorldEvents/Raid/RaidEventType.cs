ebaeab193b7d350c0e6fa88f3370077b7df05f9b
Annalurhge <annalurhge@gmail.com>
2024-08-12 11:53:01 +0800

    Initial raid event files

---
+++ Assets/Scripts/WorldEvents/Raid/RaidEventType.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UZSG.WorldEvents.Raid
+{
+    public enum RaidEventType
+    {
+        None,
+        Skinwalker,
+        Human,
+    
+    }
+}

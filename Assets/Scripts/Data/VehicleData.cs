cedd3604e50f6ee950b2d7df1a36e80f6c0200c1
jp-jp-123 <jpbeltran098@gmail.com>
2024-08-16 12:26:08 +0800

    Modified vehicle data

--- Assets/Scripts/Data/VehicleData.cs
+++ Assets/Scripts/Data/VehicleData.cs
@@ -21,10 +21,27 @@ namespace UZSG.Data
         [TextArea] public string Description;
         public AssetReference Model;
         public VehicleType Type;
-        public float AccelerationRate;
-        public float MaxSpeed;
         public float FuelConsumption;
-        
+
+        [Range(20, 190)]
+        public int maxSpeed = 90; //The maximum speed that the car can reach in km/h.
+        [Range(10, 120)]
+        public int maxReverseSpeed = 45; //The maximum speed that the car can reach while going on reverse in km/h.
+        [Space(20)]
+        public AnimationCurve powerCurve;   // Experimental Power/Torque Curve for more customized acceleration
+        public bool frontPower = true;   // Send Power to Front Wheels
+        public bool rearPower = true;   // Send Power to Rear Wheels
+        [Space(20)]
+        [Range(10, 45)]
+        public float maxSteeringAngle = 27; // The maximum angle that the tires can reach while rotating the steering wheel.
+        [Range(0.1f, 1f)]
+        public float steeringSpeed = 0.5f; // How fast the steering wheel turns.
+        [Space(20)]
+        [Range(100, 600)]
+        public int brakeForce = 350; // The strength of the wheel brakes.
+        [Range(1, 10)]
+        public int decelerationMultiplier = 2; // How fast the car decelerates when the user is not using the throttle.
+
         [Header("Vehicle Attributes")]
         public List<AttributeData> Attributes;
     }

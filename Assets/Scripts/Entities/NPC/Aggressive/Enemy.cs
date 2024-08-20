253676ce0f5438c539daaecb96ba6786201908dd
mkas020103 <mdfl020103@gmail.com>
2024-08-20 18:01:04 +0800

    Update Enemy.cs

--- Assets/Scripts/Entities/NPC/Aggressive/Enemy.cs
+++ Assets/Scripts/Entities/NPC/Aggressive/Enemy.cs
@@ -155,7 +155,7 @@ namespace UZSG.Entities
         void InitializeAttributes()
         {
             attributes = new();
-            attributes.ReadSaveJson(defaultData.Attributes);
+            attributes.ReadSaveData(defaultData.Attributes);
             _siteRadius = Attributes.Get("zombie_site_radius").Value;
             _attackRadius = Attributes.Get("zombie_attack_radius").Value;
             _speed = Attributes.Get("move_speed").Value;
